5c89a3c29af66776592512291db07a7ce8a7884d
can3lita <lizzy.mb@gmail.com>
2020-05-18 03:21:36 -0400

    Add prompt component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -9,6 +9,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
@@ -17,13 +20,18 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { StateModule } from './state/state.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { PwaService } from './services/pwa.service';
+import { PromptComponent } from './prompt/prompt.component';
+
+const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoFormComponent,
-    TodoListComponent
+    TodoListComponent,
+    PromptComponent
   ],
   imports: [
     BrowserModule,
@@ -36,11 +44,19 @@ import { environment } from '../environments/environment';
     MatInputModule,
     MatFormFieldModule,
     MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBottomSheetModule,
     StateModule.forRoot(),
     ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  entryComponents: [
+    PromptComponent,
+  ],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: initializer, deps: [PwaService], multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
